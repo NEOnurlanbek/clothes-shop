32c7f3b2ef70df54511725be576d810e5ad2f5cd
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-07-19 04:01:59 +0900

    feat: create Schema model end projeckt enums

---
+++ apps/clothes-api/src/libs/enums/property.enum.ts
@@ -0,0 +1,34 @@
+import { registerEnumType } from '@nestjs/graphql';
+
+export enum PropertyType {
+	APARTMENT = 'APARTMENT',
+	VILLA = 'VILLA',
+	HOUSE = 'HOUSE',
+}
+registerEnumType(PropertyType, {
+	name: 'PropertyType',
+});
+
+export enum PropertyStatus {
+	ACTIVE = 'ACTIVE',
+	SOLD = 'SOLD',
+	DELETE = 'DELETE',
+}
+registerEnumType(PropertyStatus, {
+	name: 'PropertyStatus',
+});
+
+export enum PropertyLocation {
+	SEOUL = 'SEOUL',
+	BUSAN = 'BUSAN',
+	INCHEON = 'INCHEON',
+	DAEGU = 'DAEGU',
+	GYEONGJU = 'GYEONGJU',
+	GWANGJU = 'GWANGJU',
+	CHONJU = 'CHONJU',
+	DAEJON = 'DAEJON',
+	JEJU = 'JEJU',
+}
+registerEnumType(PropertyLocation, {
+	name: 'PropertyLocation',
+});
---
+++ apps/clothes-api/src/schemas/Property.model.ts
@@ -0,0 +1,116 @@
+import { Schema } from 'mongoose';
+import { PropertyLocation, PropertyStatus, PropertyType } from '../libs/enums/property.enum';
+
+const PropertySchema = new Schema(
+	{
+		propertyType: {
+			type: String,
+			enum: PropertyType,
+			required: true,
+		},
+
+		propertyStatus: {
+			type: String,
+			enum: PropertyStatus,
+			default: PropertyStatus.ACTIVE,
+		},
+
+		propertyLocation: {
+			type: String,
+			enum: PropertyLocation,
+			required: true,
+		},
+
+		propertyAddress: {
+			type: String,
+			required: true,
+		},
+
+		propertyTitle: {
+			type: String,
+			required: true,
+		},
+
+		propertyPrice: {
+			type: Number,
+			required: true,
+		},
+
+		propertySquare: {
+			type: Number,
+			required: true,
+		},
+
+		propertyBeds: {
+			type: Number,
+			required: true,
+		},
+
+		propertyRooms: {
+			type: Number,
+			required: true,
+		},
+
+		propertyViews: {
+			type: Number,
+			default: 0,
+		},
+
+		propertyLikes: {
+			type: Number,
+			default: 0,
+		},
+
+		propertyComments: {
+			type: Number,
+			default: 0,
+		},
+
+		propertyRank: {
+			type: Number,
+			default: 0,
+		},
+
+		propertyImages: {
+			type: [String],
+			required: true,
+		},
+
+		propertyDesc: {
+			type: String,
+		},
+
+		propertyBarter: {
+			type: Boolean,
+			default: false,
+		},
+
+		propertyRent: {
+			type: Boolean,
+			default: false,
+		},
+
+		memberId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'Member',
+		},
+
+		soldAt: {
+			type: Date,
+		},
+
+		deletedAt: {
+			type: Date,
+		},
+
+		constructedAt: {
+			type: Date,
+		},
+	},
+	{ timestamps: true, collection: 'properties' },
+);
+
+PropertySchema.index({ propertyType: 1, propertyLocation: 1, propertyTitle: 1, propertyPrice: 1 }, { unique: true });
+
+export default PropertySchema;

ecc2a5698582b5da31f42ef95518985e77bcf44d
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-07-21 00:03:13 +0900

    feat: created components related modul

--- apps/clothes-api/src/components/components.module.ts
+++ apps/clothes-api/src/components/components.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropertyModule } from './property/property.module';
+import { AuthModule } from './auth/auth.module';
+import { BoardArticleModule } from './board-article/board-article.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { CommentModule } from './comment/comment.module';
+import { FollowModule } from './follow/follow.module';
 
 @Module({
-  imports: [MemberModule, PropertyModule]
+  imports: [MemberModule, PropertyModule, AuthModule, BoardArticleModule, LikeModule, ViewModule, CommentModule, FollowModule]
 })
 export class ComponentsModule {}

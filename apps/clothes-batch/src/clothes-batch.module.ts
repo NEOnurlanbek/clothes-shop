f311711445196b8074956cf6bbeb153ec6847dc9
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-10-07 20:14:11 +0900

    fix : modifay batch-service database connected

--- apps/clothes-batch/src/clothes-batch.module.ts
+++ apps/clothes-batch/src/clothes-batch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClothesBatchController } from './clothes-batch.controller';
 import { ClothesBatchService } from './clothes-batch.service';
 import { ConfigModule } from '@nestjs/config'
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ ConfigModule.forRoot(),],
+  imports: [ ConfigModule.forRoot(), DatabaseModule,],
   controllers: [ClothesBatchController],
   providers: [ClothesBatchService],
 })
